b99736d2e3b1dcbc29eff4c6fb5385aa3489784c
Christophe Rosset <tophe@topheman.com>
2014-07-23 22:24:00 +0200

    removed ejs & mongodb (no need for)

--- server/config/express.js
+++ server/config/express.js
@@ -19,7 +19,6 @@ module.exports = function(app) {
   var env = app.get('env');
 
   app.set('views', config.root + '/server/views');
-  app.engine('html', require('ejs').renderFile);
   app.set('view engine', 'html');
   app.use(compression());
   app.use(bodyParser.urlencoded({ extended: false }));
